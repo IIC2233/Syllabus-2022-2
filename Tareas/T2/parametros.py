b11540cf6309c084dc08a0a01f6944d3ac255a53
Juampisaez <jpsaez2@uc.cl>
2022-10-01 14:07:22 -0300

    Tarea 2 :zombie::seedling::sunflower: UPDATE

---
+++ Tareas/T2/parametros.py
@@ -0,0 +1,39 @@
+# Los intervalos están en milisegundos
+INTERVALO_DISPARO = 2000 
+INTERVALO_SOLES_GIRASOL = 20000
+INTERVALO_TIEMPO_MORDIDA = 5000
+# El daño y la vida tienen las mismas medidas
+DANO_PROYECTIL = 5
+DANO_MORDIDA = 5
+VIDA_PLANTA = 100
+VIDA_ZOMBIE = 80
+# Número de zombies por carril
+N_ZOMBIES = 7
+# Porcentaje de ralentización
+RALENTIZAR_ZOMBIE = 0.25
+# Soles iniciales por ronda
+SOLES_INICIALES = 250
+# Número de soles generados por planta
+CANTIDAD_SOLES = 2
+# Número de soles agregados a la cuenta por recolección
+SOLES_POR_RECOLECCION = 50
+# Número de soles agregados a la cuenta por Cheatcode
+SOLES_EXTRA = 25
+# Ponderadores de escenarios
+PONDERADOR_NOCTURNO = 0.8
+PONDERADOR_DIURNO = 0.9
+# La velocidad del zombie en milisegundos
+VELOCIDAD_ZOMBIE = 5000
+# Puntaje por eliminar zombie
+PUNTAJE_ZOMBIE_DIURNO = 50
+PUNTAJE_ZOMBIE_NOCTURNO = 100
+# Costo por avanzar de ronda
+COSTO_AVANZAR = 500
+# Costo tiendas
+COSTO_LANZAGUISANTE = 50
+COSTO_LANZAGUISANTE_HIELO = 100
+COSTO_GIRASOL = 50
+COSTO_PAPA = 75
+# Caracteres de nombre usuario
+MIN_CARACTERES = 3
+MAX_CARACTERES = 15
