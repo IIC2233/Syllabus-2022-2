d02fc38183565611e43afbbac7093054d0b00829
Juampisaez <jpsaez2@uc.cl>
2022-10-29 20:50:32 -0300

    :star: Enunciado actualizado

--- Tareas/T3/scripts/cripto.py
+++ Tareas/T3/scripts/cripto.py
@@ -13,7 +13,7 @@ def desencriptar(msg : bytearray) -> bytearray:
 if __name__ == "__main__":
     # Testear encriptar
     msg_original = bytearray(b'\x05\x08\x03\x02\x04\x03\x05\x09\x05\x09\x01')
-    msg_esperado = bytearray(b'\x01\x05\x02\x05\x09\x08\x04\x09\x01\x03\x03\x05')
+    msg_esperado = bytearray(b'\x01\x05\x02\x05\x09\x03\x03\x05\x08\x04\x09\x01')
 
     msg_encriptado = encriptar(msg_original)
     if msg_encriptado != msg_esperado:
